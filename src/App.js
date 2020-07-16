97827d4c6b2d4757c3f239030fdf76ec95f3ba98
PaolaDM <orchidea.preziosa@gmail.com>
2020-07-16 22:23:55 +0100

    add some text

--- src/App.js
+++ src/App.js
@@ -7,12 +7,9 @@ export default class App extends React.Component{
     return(
       <div className="container"> <h2>This is a markdown converter with React</h2>
          <div className="App"> 
-         <textarea id="text" value="" type="text"></textarea>
-         <div id="preview">
-
-         </div>
+              <textarea id="text" value="" type="text" placeholder="type your markdown"></textarea>
+              <div id="preview"> <h3>Here your preview</h3></div>
           </div>
-
       </div>
     );
   }
