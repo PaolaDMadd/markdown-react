bd8be1f0984d3e5dde954a5fbec8d922d7afd9e1
PaolaDM <orchidea.preziosa@gmail.com>
2020-07-16 22:18:57 +0100

    add some elements textarea, preview, title

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ import React from 'react';
 export default class App extends React.Component{
   render(){
     return(
-      <div className="App"> some text test </div>
+      <div className="container"> <h2>This is a markdown converter with React</h2>
+         <div className="App"> 
+         <textarea id="text" value="" type="text"></textarea>
+         <div id="preview" />
+
+         </div>
+          </div>
+
+      </div>
     );
   }
 };
